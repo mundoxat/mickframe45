0db41eea1569a6e5257a084fb37faa9015214340
Mick <mihaingu@yahoo.com>
2026-01-03 17:21:09 +0200

    Update index-gOrejz-2.js

--- assets/index-gOrejz-2.js
+++ assets/index-gOrejz-2.js
@@ -42,7 +42,7 @@ var Qu = {
  * @license React
  * react.production.min.js
  *
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Mick 10004. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
